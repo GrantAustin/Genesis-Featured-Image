d37090dfab22e1b5eeb9cbe070e8ce6e22bcd7ec
Grant Austin <grantkneble@gmail.com>
2016-03-22 18:32:45 -0400

    Basic Plugin Feature
    Simple plugin

---
+++ featured-image.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: I Hate Metaboxes 
+ * Plugin URI: http://GrantAustin.org
+ * Description: This plugin removes metaboxes that get outlandish. Best for The EDM Family (and other multi-user blogs.
+ * Version: 1.0.6
+ * Author: Grant Austin
+ * Author URI: http://GrantAustin.org
+ * License: GPL2
+ */
+
+function featured_post_image()  {
+	if ( ! is_singular( 'post' ) )  return;
+		the_post_thumbnail('post-image');
+	}
+add_action( 'genesis_before_entry', 'featured_post_image', 10 );
